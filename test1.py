da8db3f17827d73a70c25f3471346d37e493dda9
jeonghankm <jeonghank@daum.net>
2021-06-10 22:48:54 +0900

    Add Text File [test1.py]

--- test1.py
+++ test1.py
@@ -1,7 +1,3 @@
-a, b = map(int, input().split())
-if a > b:
-    print(">")
-elif a < b:
-    print("<")
-else:
-    print("==")
+a = int(input())
+for i in range(1, 10):  # 1부터 9까지
+    print(a, "x", i, '=', a * i)
