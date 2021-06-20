220face469f2006f34862e9825430a43c3127671
jeonghankm <jeonghank@daum.net>
2021-06-20 23:00:27 +0900

    while_1.py

---
+++ while_1.py
@@ -0,0 +1,12 @@
+while True:
+    a = int(input())
+    b = int(input())
+    if a == 0 and b == 0:
+        break
+    elif 0 < a < 10 and 0 < b < 10:
+        print(a+b)
+    else:
+        break
+
+
+
